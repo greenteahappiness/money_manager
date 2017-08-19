8cdc92d81343075c7ded46a10fd6a07e87023dde
Michał Szymański <smiszym@gmail.com>
2017-08-19 18:08:47 +0200

    Add Currency class for conversions

---
+++ src/main/java/monikamichael/money_manager/engine/Currency.java
@@ -0,0 +1,13 @@
+package monikamichael.money_manager.engine;
+
+public class Currency {
+    public static int parseString(String value) {
+        return (int)(Float.parseFloat(value)*100.0);
+    }
+
+    public static String toString(int value) {
+        int zl = value / 100;
+        int gr = value % 100;
+        return String.format("%d,%02d zł", zl, gr);
+    }
+}
