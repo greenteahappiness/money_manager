6b0b29942d68eaddadb55f4e37a4120cee57d756
Michał Szymański <smiszym@gmail.com>
2017-08-19 18:34:16 +0200

    Make Entry fields public

--- src/main/java/monikamichael/money_manager/engine/Entry.java
+++ src/main/java/monikamichael/money_manager/engine/Entry.java
@@ -5,8 +5,8 @@ import java.util.List;
 // An entry is a financial entry in tables.
 // It is a pair (description, value).
 public class Entry {
-    String description;
-    int value;
+    public String description;
+    public int value;
 
     public static int sum(List<Entry> entries) {
         int result = 0;
