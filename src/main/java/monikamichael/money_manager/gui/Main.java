e8654bf1787b825e11fb2a4669a89a3589d2bbab
Monia <monia.monika10@gmail.com>
2017-09-24 18:15:13 +0200

    Add logging

--- src/main/java/monikamichael/money_manager/gui/Main.java
+++ src/main/java/monikamichael/money_manager/gui/Main.java
@@ -1,14 +1,15 @@
 package monikamichael.money_manager.gui;
 
 import org.gnome.gtk.*;
-
-import java.io.FileNotFoundException;
-import java.text.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
         Gtk.init(args);
+        logger.info("MoneyManager initialized");
         HomePage homePage = new HomePage();
         homePage.show();
         Gtk.main();
