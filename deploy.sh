177856d05bab41ec2e55ae3534aa5ee2f33e9e26
Michał Szymański <smiszym@gmail.com>
2018-06-08 13:05:20 +0200

    Add support for Jetty for serving the application during development

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Note: this script is for deploying the release to Tomcat.
+# You can use Jetty for development; just run
+#    $ mvn jetty:run
+# and navigate your browser to http://localhost:8080/
+#
+
 if mvn clean compile package -DskipTests >error.log
 then
   sudo cp target/money_manager-1.0-SNAPSHOT.war /var/lib/tomcat8/webapps/
