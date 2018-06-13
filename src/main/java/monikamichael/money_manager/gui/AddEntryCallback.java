024de6e4bb1265831116ac29d229228957f7a35b
Monia <monia.monika10@gmail.com>
2018-06-13 16:11:24 +0200

    Delete gtk components

--- src/main/java/monikamichael/money_manager/gui/AddEntryCallback.java
+++
@@ -1,7 +0,0 @@
-package monikamichael.money_manager.gui;
-
-import monikamichael.money_manager.engine.Entry;
-
-public interface AddEntryCallback {
-    void onDataAvailable(int year, int month, Entry entry);
-}
--- src/main/java/monikamichael/money_manager/gui/EnterMonthDataCallback.java
+++
@@ -1,7 +0,0 @@
-package monikamichael.money_manager.gui;
-
-import monikamichael.money_manager.engine.MonthData;
-
-public interface EnterMonthDataCallback {
-    void onDataAvailable(MonthData data, int year, int month, boolean shouldCopyPeriodicExpenses);
-}
